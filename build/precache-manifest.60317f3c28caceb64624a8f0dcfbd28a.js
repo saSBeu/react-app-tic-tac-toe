6960da6625ff22aede9b69a106db0399cfbccba0
Sascha Beuse <41108921+saSBeu@users.noreply.github.com>
2019-06-30 22:09:29 +0200

    Add files via upload

---
+++ build/precache-manifest.60317f3c28caceb64624a8f0dcfbd28a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03cdc0b377ac64c16d672b7da8662e48",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2ded759307a92a914805",
+    "url": "/static/css/main.55d72bbf.chunk.css"
+  },
+  {
+    "revision": "58f496bc1ce27350f0eb",
+    "url": "/static/js/2.98dcc9dd.chunk.js"
+  },
+  {
+    "revision": "2ded759307a92a914805",
+    "url": "/static/js/main.8db80129.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
